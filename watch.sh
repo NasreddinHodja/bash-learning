893d5e5f4655841e68ac719ec543c73ea8f1436d
tbizetde@gmail.com <tbizetde@gmail.com>
2025-09-20 20:23:50 -0300

    ./watch.sh reloads when files are deleted/renamed

--- watch.sh
+++ watch.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env sh
 
-(find notes static build.sh watch.sh publish.el -type f | entr ./build.sh) &
-WATCH_PID=$!
+(while inotifywait -r -e modify,create,delete,move notes static build.sh watch.sh publish.el; do
+    ./build.sh
+done) &
 
+WATCH_PID=$!
 trap "kill $WATCH_PID" EXIT
 
 browser-sync start --server public --files "public/**/*.*" --no-open --reload-delay 50
